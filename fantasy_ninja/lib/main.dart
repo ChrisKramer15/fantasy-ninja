3e7c5668ad6378a609d2088293945f42c8926393
Chris Kramer <chris@vetaip.com>
2022-06-28 20:43:17 -0500

    background added to home screen

--- fantasy_ninja/lib/main.dart
+++ fantasy_ninja/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
---
+++ fantasy_ninja/lib/utils/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaletteColor {
+  static Color mainColor = const Color(0xff257c6a);
+  static Color darkAccent = const Color(0xff408cc1);
+  static Color lightAccent = const Color(0xffc93129);
+  static Color lightShade = const Color(0xfff0ecee);
+  static Color darkShade = const Color(0xff16161f);
+}
